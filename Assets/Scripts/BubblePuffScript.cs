aa248ee079ed68b6187ff210b772504bae3a77af
Angela Ku <angku88@gmail.com>
2024-06-13 17:02:58 -0700

    Enable instruments as tempo increases

--- Assets/Scripts/BubblePuffScript.cs
+++ Assets/Scripts/BubblePuffScript.cs
@@ -17,9 +17,23 @@ public class BubblePuffScript : MonoBehaviour
 
             if (GameMultiplier.gameTempo < 100) {
                 GameMultiplier.gameTempo = 100;
+
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_d", 1);
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_e", 1);
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_f", 1);
             }
 
             gameObject.SetActive(false);
+
+            if (GameMultiplier.gameTempo <= 120) {
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/drum_heavy", 1);
+            } 
+            
+            if (GameMultiplier.gameTempo <= 110) {
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_c", 1);
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_g", 1);
+                OSCHandler.Instance.SendMessageToClient ("pd", "/unity/note_a", 1);
+            }
 
             //************* Send the message to the client...
             OSCHandler.Instance.SendMessageToClient ("pd", "/unity/one_timbre", GameMultiplier.oneTimbre);
